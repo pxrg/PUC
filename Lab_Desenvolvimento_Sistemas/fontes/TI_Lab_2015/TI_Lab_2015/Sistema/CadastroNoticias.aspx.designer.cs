3b09a64a1162c50839a299d78d256c6d52d6048c
unknown <prxgomes@gmail.com>
2015-03-28 12:56:35 -0300

    Telas principal e de noticias complemento

---
+++ Lab_Desenvolvimento_Sistemas/fontes/TI_Lab_2015/TI_Lab_2015/Sistema/CadastroNoticias.aspx.designer.cs
@@ -0,0 +1,33 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     O código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código é regenerado. 
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace TI_Lab_2015.Sistema {
+    
+    
+    public partial class CadastroNoticias {
+        
+        /// <summary>
+        /// Controle txtNome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNome;
+        
+        /// <summary>
+        /// Controle TextBox2.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+    }
+}
